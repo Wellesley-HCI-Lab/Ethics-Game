be7e9277586057d30f80a978db9c6c8b6fc7cd5e
Jen-Otiono <jotiono@wellesley.edu>
2018-07-12 16:04:58 -0400

    started splash scene

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,17 @@
 Creating a new game and adding states
 */
 
-var game = new Phaser.Game(2048, 1536, Phaser.AUTO, '', { create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { create: create });
 
 // Main boot screen / splash screen
 game.state.add('boot', bootState);
 // Loads all assets (with a progress bar or something)
 game.state.add('load', loadState);
+
+
+/**
+ * Function that runs right after the world is created
+ */ 
 
 function create() { 
 	game.state.start('boot'); 
