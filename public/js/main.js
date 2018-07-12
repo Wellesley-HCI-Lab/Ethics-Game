c4641322b54f5620d3069ce61f76bb134a444ff9
p-koul <pkoul@wellesley.edu>
2018-07-12 13:45:28 -0400

    Added more folders to organize images

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,10 @@ var game = new Phaser.Game(2048, 1536, Phaser.AUTO, '', { create: create });
 
 // Main boot screen / splash screen
 game.state.add('boot', bootState);
-// Loads all assets (with a progress bar or something)
+// Loads all assets 
 game.state.add('load', loadState);
+// Loads state with the main game loop
+game.state.add('game', mainGameState);
 
 function create() { 
 	game.state.start('boot'); 
