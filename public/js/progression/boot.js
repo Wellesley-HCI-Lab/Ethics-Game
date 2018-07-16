1083fbe434497f2c6567bf274e4ecc40cb9ae292
Jen-Otiono <jotiono@wellesley.edu>
2018-07-16 14:17:02 -0400

    check

--- public/js/progression/boot.js
+++ public/js/progression/boot.js
@@ -38,7 +38,7 @@ var BootState = (function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         background = game.add.tileSprite(0, 0, 800, 600, 'background');
-        porthole = game.add.sprite(0, 0, 400, 300, 'porthole');
+        porthole = game.add.sprite(800, 600, 'porthole');
         button = game.add.button(game.world.centerX-95,400, 'button', callLoad, this, 2, 1, 0);
     }
     
