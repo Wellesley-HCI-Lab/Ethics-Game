e3c6bba3637e91314e65f73f9621877328d82b78
Jen-Otiono <jotiono@wellesley.edu>
2018-07-16 18:54:38 -0400

    check

--- public/js/progression/boot.js
+++ public/js/progression/boot.js
@@ -39,7 +39,7 @@ var BootState = (function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         //background = game.add.tileSprite(0, 0, 800, 600, 'background');
-        underwaterbac = game.add.tileSprite(0, 0, 800, 400, 'underwaterbac');
+        underwaterbac = game.add.tileSprite(0, 0, 700, 300, 'underwaterbac');
         porthole = game.add.tileSprite(0, 0, 800, 600, 'porthole');
         button = game.add.button(game.world.centerX-95,400, 'button', callLoad, this, 2, 1, 0);
     }
