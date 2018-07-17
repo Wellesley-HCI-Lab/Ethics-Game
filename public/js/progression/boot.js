6c4af4722c02ae0787562c4f66ba65bf40ef1c8e
Jen-Otiono <jotiono@wellesley.edu>
2018-07-17 00:57:38 -0400

    check

--- public/js/progression/boot.js
+++ public/js/progression/boot.js
@@ -26,8 +26,10 @@ var BootState = (function() {
     var load = function(){ 
         game.load.image('background', 'images/background/background.png');
         game.load.image('button', 'images/background/button.png');
-        game.load.image('porthole', 'images/background/portholeSub.png')
-        game.load.image('underwaterbac', 'images/background/underwater.png')
+        game.load.image('porthole', 'images/background/portholeSub.png');
+        game.load.image('underwaterbac', 'images/background/underwater.png');
+        game.load.image('ang', 'images/background/ang.png');
+        game.load.image('ball', 'images/background/bubble256.png');
     }
     
     var create = function(){
@@ -39,15 +41,69 @@ var BootState = (function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         //background = game.add.tileSprite(0, 0, 800, 600, 'background');
-        underwaterbac = game.add.tileSprite(0, 0, 700, 300, 'underwaterbac');
+        
+        underwaterbac = game.add.tileSprite(game.world.centerX-180, -150, 400, 600, 'underwaterbac');
+        
+        
+        ang = game.add.sprite(300,200,'ang');
+        ang2 = game.add.sprite(250,300,'ang');
+        ang3 = game.add.sprite(400,200,'ang');
+
+        ang.scale.setTo(0.1, 0.1);
+        ang2.scale.setTo(0.2, 0.2);
+        ang2.scale.setTo(0.3, 0.3);
+
+        ang.alpha = 0;
+        ang2.alpha = 0;
+        ang3.alpha = 0;
+
+        game.add.tween(ang).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+        game.add.tween(ang2).to( { alpha: 1 }, 4000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+        game.add.tween(ang3).to( { alpha: 1 }, 6000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+        ang3.inputEnabled = true;
+        ang3.events.onInputDown.add(bubbleBurst);
+
+        console.log(game.world.centerY);
+        
         porthole = game.add.tileSprite(0, 0, 800, 600, 'porthole');
-        button = game.add.button(game.world.centerX-95,400, 'button', callLoad, this, 2, 1, 0);
+
+       
+
+
+        button = game.add.button(game.world.centerX-120,400, 'button', callLoad, this, 2, 1, 0);
+
+
     }
-    
+
+     
+    var bubbleBurst = function(){
+        var delay = 0;
+
+        for (var i = 0; i < 5; i++){
+
+        bubbles = game.add.sprite(410 , 300, 'ball');
+        //bubbles.anchor.setTo(0.00001, 0.00001);
+        bubbles.scale.set(game.rnd.realInRange(0.01, 0.2));
+        speed = game.rnd.between(4000, 6000);
+        game.add.tween(bubbles).to({ y: 100 }, speed, Phaser.Easing.Sinusoidal.InOut, true, delay, 6000, false);
+
+        delay += 200;
+        console.log('fuck me');
+
+
+        }
+
+    }
+
+
     var callLoad = function(){
         game.state.start('load');
         console.log('move state to game');
     }
+
+    
+
+
 
     return {
         load: load,
